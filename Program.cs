9254bf698e6e92157753fff71c74a673704426d8
«Apolon» <hertek1990@gmail.com>
2023-02-14 22:27:58 +0700

    Zadacha 4

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿System.Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    if (number1 > number3)
+    {
+        System.Console.WriteLine("MAX"+number1);
+    }
+    else 
+    {
+       System.Console.WriteLine("MAX"+number3); 
+    }
+}
+else 
+{
+    if (number2 > number3)
+    {
+        System.Console.WriteLine("MAX"+number2);
+    }
+    else 
+    {
+       System.Console.WriteLine("MAX"+number3); 
+    }
+}
